5148c66f5417074fe30fa5552c8350113ca4693c
Muhammad Nur Fauzan <60289157+Yashiro-San@users.noreply.github.com>
2020-10-21 16:26:30 +0800

    Update and rename lynda/modules/paste.py to chizuru/modules/paste.py

--- chizuru/modules/paste.py
+++ chizuru/modules/paste.py
@@ -2,8 +2,8 @@ import requests
 from telegram import Update, ParseMode
 from telegram.ext import run_async, CallbackContext
 
-from lynda import dispatcher
-from lynda.modules.disable import DisableAbleCommandHandler
+from chizuru import dispatcher
+from chizuru.modules.disable import DisableAbleCommandHandler
 
 
 @run_async
