92489fbfd5a3fa1cbf65c173d0e5fe5c75402825
Muhammad Nur Fauzan <60289157+Yashiro-San@users.noreply.github.com>
2020-10-22 09:03:45 +0800

    Update and rename lynda/modules/sql/log_channel_sql.py to chizuru/modules/sql/log_channel_sql.py

--- chizuru/modules/sql/log_channel_sql.py
+++ chizuru/modules/sql/log_channel_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, func, distinct
 
-from lynda.modules.sql import BASE, SESSION
+from chizuru.modules.sql import BASE, SESSION
 
 
 class GroupLogs(BASE):
