6a9091c7926a409a71a16244305fada9143064e7
Muhammad Nur Fauzan <60289157+Yashiro-San@users.noreply.github.com>
2020-10-22 08:58:33 +0800

    Update and rename lynda/modules/sql/connection_sql.py to chizuru/modules/sql/connection_sql.py

--- chizuru/modules/sql/connection_sql.py
+++ chizuru/modules/sql/connection_sql.py
@@ -4,7 +4,7 @@ from typing import Union
 
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer
 
-from lynda.modules.sql import SESSION, BASE
+from chizuru.modules.sql import SESSION, BASE
 
 
 class ChatAccessConnectionSettings(BASE):
