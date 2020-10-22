ce1f8172069682ecab80f49f3ae379bf9e63a844
Muhammad Nur Fauzan <60289157+Yashiro-San@users.noreply.github.com>
2020-10-22 08:59:18 +0800

    Update and rename lynda/modules/sql/cust_filters_sql.py to chizuru/modules/sql/cust_filters_sql.py

--- chizuru/modules/sql/cust_filters_sql.py
+++ chizuru/modules/sql/cust_filters_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Boolean, Integer, distinct, func
 
-from lynda.modules.sql import BASE, SESSION
+from chizuru.modules.sql import BASE, SESSION
 
 
 class CustomFilters(BASE):
